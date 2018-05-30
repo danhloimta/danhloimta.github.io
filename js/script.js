f376e8e30bb7f27de8ff9e97e41764cbcb0dbfb2
danhloimta <nobitaa4@gmail.com>
2018-05-30 15:24:03 +0700

    a

--- js/script.js
+++ js/script.js
@@ -20,32 +20,32 @@ $(document).ready(function(){
 
 
 
-    $('.menu ul li:nth-child(x) a').on('click', function(e){
-     	$('html,body').animate({scrollTop: $('header').offset().top},400, 'easeOutExpo');
-     	$('.menu ul li').removeClass('active');
-     	$(this).addClass('active');
-     	return false;
-     });
+    // $('.menu ul li:nth-child(x) a').on('click', function(e){
+    //  	$('html,body').animate({scrollTop: $('header').offset().top},400, 'easeOutExpo');
+    //  	$('.menu ul li').removeClass('active');
+    //  	$(this).addClass('active');
+    //  	return false;
+    //  });
 
 
-	// $('.menu ul li:nth-child(2) a').on('click', function(e){
-	//      	$('html,body').animate({scrollTop: $('.about').offset().top-100},400, 'easeOutExpo');
-	//      	return false;
-	//      });
-	// $('.menu ul li:nth-child(3) a').on('click', function(e){
-	//      	$('html,body').animate({scrollTop: $('.education').offset().top},400, 'easeOutExpo');
-	//      	return false;
-	//      });
-	// $('.menu ul li:nth-child(4) a').on('click', function(e){
-	//      	$('html,body').animate({scrollTop: 1899},400, 'easeOutExpo');
-	//      	return false;
-	//      });
+	$('.menu ul li:nth-child(2) a').on('click', function(e){
+	     	$('html,body').animate({scrollTop: $('.about').offset().top-100},400, 'easeOutExpo');
+	     	return false;
+	     });
+	$('.menu ul li:nth-child(3) a').on('click', function(e){
+	     	$('html,body').animate({scrollTop: $('.education').offset().top},400, 'easeOutExpo');
+	     	return false;
+	     });
+	$('.menu ul li:nth-child(4) a').on('click', function(e){
+	     	$('html,body').animate({scrollTop: 1899},400, 'easeOutExpo');
+	     	return false;
+	     });
 
-	// $('i.fas.fa-angle-down').on('click', function () {
-	// 	$('html,body').animate({scrollTop: 650},400, 'easeOutExpo');
-	// })
+	$('i.fas.fa-angle-down').on('click', function () {
+		$('html,body').animate({scrollTop: 650},400, 'easeOutExpo');
+	})
 
 
-	// TweenMax.staggerFrom($('ul.nav.main-menu li'), 0.8,{top:-150, opacity:0},0.4);
+	TweenMax.staggerFrom($('ul.nav.main-menu li'), 0.8,{top:-150, opacity:0},0.4);
 
     });
